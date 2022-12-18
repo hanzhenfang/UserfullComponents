23ce1a8efda3d47c221fa4e70376ca15e584319b
hanzhenfang <hanzhenfffang@gmail.com>
2022-12-18 15:14:05 +0800

    feat: first commit
    Signed-off-by: hanzhenfang <hanzhenfffang@gmail.com>

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from "vue"
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="w-100px h-100px text-yellow border-red border-1px">哈哈</div>
+</template>
+
+<style scoped></style>
---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+
+import Unocss from "unocss/vite"
+import presetUno from "@unocss/preset-uno"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), Unocss({ presets: [presetUno()] })],
+})

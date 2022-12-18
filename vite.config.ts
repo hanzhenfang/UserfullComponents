2fad9771741854dcd61d7ef9cc64a9ff959ee742
hanzhenfang <hanzhenfffang@gmail.com>
2022-12-18 15:27:13 +0800

    chore: clean the workspace

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import vue from "@vitejs/plugin-vue"
 import Unocss from "unocss/vite"
 import presetUno from "@unocss/preset-uno"
 
+import path from "path"
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), Unocss({ presets: [presetUno()] })],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(process.cwd(), "src"),
+    },
+  },})

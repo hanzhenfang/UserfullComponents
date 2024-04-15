ca780773f311abfa19a13220ad9f51b43fd80110
hanzhenfang <hanzhenfffang@gmail.com>
2024-04-15 15:27:21 +0800

    feat: add a new format time fn

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -17,3 +17,42 @@ export function formattingTime(count: number) {
   }
   return time
 }
+
+/**
+ *
+ * @param number
+ * @returns "xx小时xx分钟xx秒"
+ */
+export function formatSeconds(value: number) {
+  let theTime = value // 需要转换的时间秒
+  let theTime1 = 0 // 分
+  let theTime2 = 0 // 小时
+  let theTime3 = 0 // 天
+  if (theTime > 60) {
+    theTime1 = parseInt(theTime / 60 + "")
+    theTime = parseInt((theTime % 60) + "")
+    if (theTime1 > 60) {
+      theTime2 = parseInt(theTime1 / 60 + "")
+      theTime1 = parseInt((theTime1 % 60) + "")
+      if (theTime2 > 24) {
+        //大于24小时
+        theTime3 = parseInt(theTime2 / 24 + "")
+        theTime2 = parseInt((theTime2 % 24) + "")
+      }
+    }
+  }
+  let result = ""
+  if (theTime > 0) {
+    result = "" + parseInt(theTime + "") + "秒"
+  }
+  if (theTime1 > 0) {
+    result = "" + parseInt(theTime1 + "") + "分" + result
+  }
+  if (theTime2 > 0) {
+    result = "" + parseInt(theTime2 + "") + "小时" + result
+  }
+  if (theTime3 > 0) {
+    result = "" + parseInt(theTime3 + "") + "天" + result
+  }
+  return result
+}
